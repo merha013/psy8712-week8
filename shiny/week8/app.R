e721420b32b968ed04a0152b926f687289f51116
Toni Merhar <merha013@umn.edu>
2024-03-14 21:47:25 -0500

    Part 2 mostly done. Working on Part 3 still, but there is something wrong with my skinny_tbl file.

--- shiny/week8/app.R
+++ shiny/week8/app.R
@@ -1,17 +1,20 @@
 library(shiny)
 library(tidyverse)
-#week8_skinny_tbl <- readRDS("week8_skinny.RDS")
+skinny_tbl <- readRDS("../shiny/week8/skinny.RDS")
 
 ui <- fluidPage(
     titlePanel("shiny_week8"),  # Application title
     sidebarLayout(
       sidebarPanel(
+        # add radio buttons to select gender
         radioButtons("Gender",
                      "I would like to display:",
                      choices = c("All", "Male", "Female")),
+        # add radio buttons to select error band or not
         radioButtons("Error Band",
                      "I would like to:",
                      choices = c("Display Error Band", "Suppress Error Band")),
+        # add radio buttons to include early participants or not
         radioButtons("Participants",
                     "Would you like to include participants that completed 
                     the assessment before July 1, 2017?",
@@ -26,13 +29,15 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$week8Plot <- renderPlot({
-        ggplot(week8_skinny_tbl, aes(x=average1, y=average2)) +
+      sliced_tbl <- skinny_tbl %>%
+        filter(gender == input$Gender) 
+      ggplot(sliced_tbl, aes(x=average1, y=average2)) +
         geom_point() +
-        geom_smooth(method="lm", color="purple") +
+        geom_smooth(method="lm", se=TRUE, color="purple") +
         labs(title = "Scatterplot between Mean Scores",
              x = "Mean Scores on Q1-Q6",
              y = "Mean Scores on Q8-Q10")
-      })
+    })
 }
 
 # Run the application 
