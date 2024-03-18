b042286f721b828deb6f58316a81485e2d292a3d
Toni Merhar <merha013@umn.edu>
2024-03-17 20:32:51 -0500

    Reviewed, updated, and completed.

--- shiny/week8/app.R
+++ shiny/week8/app.R
@@ -1,30 +1,35 @@
 library(shiny)
 library(tidyverse)
+library(rsconnect)
 
 ui <- fluidPage(
-  # created a title called shiny_week8 in both the shinyapps account and in the URL to open it
+  # I made shiny_week8 the title in the shinyapps account and the URL.
   titlePanel("shiny_week8"),
   sidebarLayout(
     sidebarPanel(
-      # I used radio buttons for all three options for consistency
-      # add radio buttons to select which gender to display
-      # placing "All" at the beginning of the choice options makes it the default
+      # I used radio buttons for all three options for consistency.
+      # I created radio buttons to select which gender to display, and
+      # placing "All" at the beginning of the choice options makes it
+      # the default.
       radioButtons("Gender",
                    "I would like to display:",
                    choices = c("All", "Male", "Female")),
-      # add radio buttons to select whether or not to display the error band
-      # placing "Display Error Band" at the beginning of the choice options makes it the default
+      # I added radio buttons to select whether or not to display the 
+      # error band. Placing "Display Error Band" at the beginning of 
+      # the choice options makes it the default.
       radioButtons("Errorband",
                    "I would like to:",
                    choices = c("Display Error Band", "Supress Error Band")),
-      # add radio buttons to select participants prior to July 2017 or not
-      # placing "Yes" at the beginning of the choice options makes it the default
+      # I added radio buttons to select participants prior to July 2017 
+      # or not. Placing "Yes" at the beginning of the choice options makes 
+      # it the default.
       radioButtons("Time",
-                   "Would you like to include participants that completed the assessment before July 1, 2017?",
+                   "Would you like to include participants that completed 
+                   the assessment before July 1, 2017?",
                    choices = c("Yes", "No"))
     ),
     mainPanel(
-      # define what the output should be referenced as
+      # I defined week8plot as what the output should be referenced as.
       plotOutput("week8plot")
     )
   )
@@ -34,35 +39,38 @@ server <- function(input, output) {
   
   output$week8plot <- renderPlot({
   
-    # read the minimal/skinny version of the dataset appropriate for shiny (saved in Part 2)
+    # I read the minimal/skinny version of the dataset appropriate 
+    # for shiny (saved in Part 2) inside the renderPlot() function.
     skinny_tbl <- readRDS("skinny.rds")
   
-    # Since "All" is not an option in the gender column, 
-    # create an if(){} code that allows "All" to keep the full dataset
-    # but then keeps only Male or Female in the dataset if they are selected
+    # Since "All" is not an option in the gender column, I
+    # created an if(){} code that allows "All" to keep the full dataset
+    # but then keeps only Male or Female data if they are selected.
     if(input$Gender!="All"){
       skinny_tbl <- skinny_tbl %>%
         filter(gender == input$Gender)
     }
     
-    # create an if(){} code that excludes participants who completed the 
-    # assessment before July 1, 2017 if "No" is selected in the radio buttons
+    # I created an if(){} code that excludes participants who completed 
+    # the assessment before (timeEnd) July 1, 2017 if "No" is selected 
+    # in the radio buttons. 
     if(input$Time=="No"){
       skinny_tbl <- skinny_tbl %>%
         filter(timeEnd>"2017-06-30 23:59:59")
     }
 
-    # create the plot to show the data selected
+    # I re-created the same plot from the .rmd file to show the data selected.
     skinny_tbl %>%
       ggplot(aes(x=average1, y=average2)) +
       geom_point() +
       geom_smooth(method="lm", color="purple", 
-                  # only display the error bands if "Display Error Band" 
-                  # is selected in the radio buttons
+                  # Is used an if(){}ele{} statement to only display the 
+                  # error bands if "Display Error Band" is selected in 
+                  # the radio buttons.
                   se=if(input$Errorband=="Display Error Band"){TRUE}else{FALSE}) +
-      labs(title = "Scatterplot between Mean Scores",
-           x = "Mean Scores on Q1-Q6",
-           y = "Mean Scores on Q8-Q10")
+      labs(title = "Scatterplot of Mean Scores",
+           x = "Mean Scores of Q1-Q6",
+           y = "Mean Scores of Q8-Q10")
   })
 }
 
